23d1cfc0341078f217e688206c78dfa72efbc53a
Maurice Makaay <mmakaay1@xs4all.net>
2021-09-13 13:42:12 +0200

    Don't to CRC checking (just a test).

--- esphome/components/dsmr/dsmr.cpp
+++ esphome/components/dsmr/dsmr.cpp
@@ -131,7 +131,7 @@ bool Dsmr::parse_telegram() {
   ESP_LOGV(TAG, "Trying to parse");
   ::dsmr::ParseResult<void> res =
       ::dsmr::P1Parser::parse(&data, telegram_, telegram_len_,
-                              false);  // Parse telegram according to data definition. Ignore unknown values.
+                              false, false);  // Parse telegram according to data definition. Ignore unknown values.
   if (res.err) {
     // Parsing error, show it
     auto err_str = res.fullError(telegram_, telegram_ + telegram_len_);
